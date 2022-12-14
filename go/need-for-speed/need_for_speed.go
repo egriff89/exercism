dc910c6a0c7a0c351de9662e8b8908cf2d400c06
Eric Griffith <eric.griffith42@gmail.com>
2022-12-13 22:46:39 -0600

    go: need for speed

---
+++ go/need-for-speed/need_for_speed.go
@@ -0,0 +1,56 @@
+package speed
+
+type Car struct {
+	battery      int
+	batteryDrain int
+	speed        int
+	distance     int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{
+		battery:      100,
+		batteryDrain: batteryDrain,
+		speed:        speed,
+		distance:     0,
+	}
+}
+
+type Track struct {
+	distance int
+}
+
+// NewTrack creates a new track
+func NewTrack(distance int) Track {
+	return Track{
+		distance: distance,
+	}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive one more time,
+// the car will not move.
+func Drive(car Car) Car {
+	if car.battery < car.batteryDrain {
+		return car
+	}
+
+	newBattery := car.battery - car.batteryDrain
+	newDistance := car.distance + car.speed
+
+	return Car{
+		speed:        car.speed,
+		batteryDrain: car.batteryDrain,
+		battery:      newBattery,
+		distance:     newDistance,
+	}
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	distRemaining := track.distance - car.distance
+	laps := distRemaining / car.speed
+	expendedBattery := laps * car.batteryDrain
+
+	return expendedBattery <= car.battery
+}

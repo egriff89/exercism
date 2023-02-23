af3a9ac93668d4ac7a6a8e4009e2a926bc444362
Eric Griffith <eric.griffith42@gmail.com>
2023-02-22 21:28:50 -0600

    Rust: assembly line

---
+++ rust/assembly-line/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    let cars_per_hour: f64 = 221.0;
+    let rate = (speed as f64) * cars_per_hour;
+
+    return match speed {
+        (1..=4)  => rate,
+        (5..=8)  => rate * 0.90,
+        (9..=10) => rate * 0.77,
+        _ => 0.0
+    };
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    let actual_rate: f64 = production_rate_per_hour(speed) * 100.0;
+    return ((actual_rate.round() as u32) / 100) / 60;
+}

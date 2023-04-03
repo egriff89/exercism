654d9591fb584de44d69854231740f8382f9de3d
Eric Griffith <eric.griffith42@gmail.com>
2023-04-03 16:33:46 -0500

    rust: difference of squares

---
+++ rust/difference-of-squares/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn square_of_sum(n: u32) -> u32 {
+    return (1..=n).sum::<u32>().pow(2)
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    return match n {
+        1 => 1,
+        _ => (1..=n).map(|x: u32| x.pow(2)).sum()
+    }
+}
+
+pub fn difference(n: u32) -> u32 {
+    return square_of_sum(n) - sum_of_squares(n)
+}

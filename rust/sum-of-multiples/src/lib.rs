5ec5e3568168df633bdfaf7140a1bff3b39babd1
Eric Griffith <eric.griffith42@gmail.com>
2023-04-04 00:13:27 -0500

    refactor 'sum of multiples' to filter over iterators instead of using if blocks and loops

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,28 +1,8 @@
-use std::collections::HashSet;
-
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let mut uniques: HashSet<u32> = HashSet::new();
-
-    if (factors.len() == 1 && factors[0] == 0) || factors.len() == 0 {
-        return 0
-    } else {
-
-        // Calculate factors (ugly, but works)
-        for &f in factors {
-            if f == 0 {
-                continue;
-            } else {
-                for j in 0.. {
-                    let res: u32 = f * j;
-                    if res >= limit {
-                        break;
-                    } else {
-                        uniques.insert(res);
-                    }
-                }
-            }
-        }
-    }
-
-    return uniques.iter().sum()
+    return (0..limit)
+        .filter(|&n| 
+            factors
+                .iter()
+                .any(|&f| (f != 0) && (n % f == 0)))
+        .sum()
 }

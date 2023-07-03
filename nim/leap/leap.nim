f1c359040bbdb5bc915af9f38d26d34c53d7a67c
Eric Griffith <eric.griffith42@gmail.com>
2023-07-03 09:34:55 -0500

    refactor leap.nim

--- nim/leap/leap.nim
+++ nim/leap/leap.nim
@@ -2,10 +2,8 @@ proc divisible(year: int, number: int): bool =
    return year mod number == 0
 
 proc isLeapYear*(year: int): bool =
-   if divisible(year, 4) and divisible(year, 100):
-      if divisible(year, 400):
-         return true
-      return false
-   elif divisible(year, 4):
+   if divisible(year, 400):
       return true
-   return false
+   elif divisible(year, 100):
+      return false
+   return divisible(year, 4)

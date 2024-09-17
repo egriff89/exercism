927c2dd857b651d0fa003bf01301e63a21869e16
Eric Griffith <eric.griffith42@gmail.com>
2024-09-17 13:46:50 -0500

    refactor leap.py

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,14 +1,16 @@
-""" Given a year, report if it is a leap year. """
+"""Given a year, report if it is a leap year."""
+
 
 def is_even(year: int, div: int) -> bool:
-    """ Validate year is evenly divisible """
+    """Validate year is evenly divisible"""
     return year % div == 0
 
+
 def leap_year(year: int) -> bool:
-    """ Validate leap year """
-    if is_even(year, 4) and is_even(year, 100):
-        if is_even(year, 400):
-            return True
+    """Validate leap year"""
+    if is_even(year, 400):
+        return True
+    if is_even(year, 100):
         return False
     if is_even(year, 4):
         return True

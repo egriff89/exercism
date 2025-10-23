218f7ba546ed9bd545c3472d805b60f99d918bfb
Eric Griffith <griffith.eric@pm.me>
2025-10-22 23:07:14 -0500

    refactor(collatz go): minor improvements to make things cleaner

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -7,19 +7,14 @@ func CollatzConjecture(n int) (int, error) {
 		return 0, errors.New("cannot be zero or negative")
 	}
 
-	if n == 1 {
-		return 0, nil
-	}
-
 	var steps int
-	for n != 1 {
+	for n > 1 {
 		if n%2 == 0 {
 			n /= 2
-			steps += 1
 		} else {
 			n = (3 * n) + 1
-			steps += 1
 		}
+		steps += 1
 	}
 	return steps, nil
 }

a47319228bc9809c49215e76215f6f3d10239a08
Eric Griffith <griffith.eric@pm.me>
2025-11-05 11:04:28 -0600

    refactor(difference-of-squares.ml)

--- ocaml/difference-of-squares/difference_of_squares.ml
+++ ocaml/difference-of-squares/difference_of_squares.ml
@@ -1,38 +1,7 @@
-open Base
+let square_of_sum n = 
+  let sum = n * (n + 1) / 2 in
+  sum * sum
 
-(* Generate list from 1 to n *)
-let range n = List.init n ~f:Int.succ
-
-(* Sum list contents *)
-let rec sum_list = function
-  | [] -> 0
-  | h :: t -> h + sum_list t
-;;
-
-(* Square list contents *)
-let rec sq_list = function
-  | [] -> 0
-  | h :: t ->
-    let open Float.O in
-    Int.of_float (of_int h ** 2. + of_int (sq_list t))
-;;
-
-let square_of_sum = function
-  | 0 -> 0
-  | x -> 
-    let open Float.O in
-    Int.of_float (of_int (sum_list (range x)) ** 2.)
-;;
-
-let sum_of_squares = function
-  | x ->
-    let list = range x in
-    match list with
-    | [] -> 0
-    | h :: t ->
-      let open Float.O in
-      Int.of_float (of_int h ** 2. + of_int (sq_list t))
-    ;;
-;;
+let sum_of_squares n = n * (n + 1) * (2 * n + 1) / 6
 
 let difference_of_squares n = square_of_sum n - sum_of_squares n

bd25f53d0e5de70779258f211d15ea09ecb4e507
Eric Griffith <griffith.eric@pm.me>
2025-11-11 11:48:16 -0600

    refactor(scheme): difference of squares

--- scheme/difference-of-squares/difference-of-squares.scm
+++ scheme/difference-of-squares/difference-of-squares.scm
@@ -4,14 +4,10 @@
 (use-modules (srfi srfi-1))
 
 (define (square-of-sum n)
-  (let ([lst (iota n 1)])
-    (define sum (fold + 0 lst))
-    (expt sum 2)))
+  (expt (apply + (iota n 1)) 2))
 
 (define (sum-of-squares n)
-  (let ([lst (iota n 1)])
-    (define squares (map (lambda (x) (* x x)) lst))
-    (fold + 0 squares)))
+  (apply + (map (lambda (x) (* x x)) (iota n 1))))
 
 (define (difference-of-squares n)
   (- (square-of-sum n) (sum-of-squares n)))

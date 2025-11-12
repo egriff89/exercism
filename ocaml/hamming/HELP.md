d6b3070a3fd2a9ee4fab9b58147fd05727823ee5
Eric Griffith <griffith.eric@pm.me>
2025-11-11 23:42:37 -0600

    refactor(ocaml): fix hamming to work with updated tests

--- ocaml/hamming/HELP.md
+++ ocaml/hamming/HELP.md
@@ -23,6 +23,7 @@ It's possible to submit an incomplete solution which allows you to:
 If you'd like help solving the exercise, check the following pages:
 
 - The [OCaml track's documentation](https://exercism.org/docs/tracks/ocaml)
+- The [OCaml track's programming category on the forum](https://forum.exercism.org/c/programming/ocaml)
 - [Exercism's programming category on the forum](https://forum.exercism.org/c/programming/5)
 - The [Frequently Asked Questions](https://exercism.org/docs/using/faqs)
 
--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -9,16 +9,16 @@ let nucleotide_to_char(dna: nucleotide) =
   | G -> 'G'
   | T -> 'T'
 
-let hamming_distance strand_a strand_b =
+let hamming_distance (strand_a: nucleotide list ) (strand_b: nucleotide list) =
   match List.is_empty strand_a, List.is_empty strand_b with
   | (true, true) -> Ok 0
-  | (true, _) -> Error "left strand must not be empty"
-  | (_, true) -> Error "right strand must not be empty"
+  | (true, _) -> Error "strands must be of equal length"
+  | (_, true) -> Error "strands must be of equal length"
   | _ ->
     let a_len = List.length strand_a in
     let b_len = List.length strand_b in
-    if a_len <> b_len then 
-      Error "left and right strands must be of equal length"
+    if a_len  <> b_len then 
+      Error "strands must be of equal length"
     else
       let arr_a = Array.of_list strand_a |> Array.map ~f:(nucleotide_to_char) in
       let arr_b = Array.of_list strand_b |> Array.map ~f:(nucleotide_to_char) in

cb50f83e72f1c078c2d0fc539001f7c0589ff2f7
Eric Griffith <griffith.eric@pm.me>
2026-04-30 21:35:39 -0500

    common lisp: collatz conjecture

---
+++ common-lisp/collatz-conjecture/collatz-conjecture.lisp
@@ -0,0 +1,17 @@
+(defpackage :collatz-conjecture
+  (:use :cl)
+  (:export :collatz))
+
+(in-package :collatz-conjecture)
+
+(defun collatz-aux (n step)
+  (if (= 1 n)
+    step
+    (if (evenp n)
+      (collatz-aux (/ n 2) (incf step))
+      (collatz-aux (+ (* 3 n) 1) (incf step)))))
+
+(defun collatz (n) 
+  (if (< n 1)
+    nil
+    (collatz-aux n 0)))
